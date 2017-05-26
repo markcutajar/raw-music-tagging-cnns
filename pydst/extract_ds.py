6d4f156c45afab58a255f60a5c0c0bc088bc71ed
Mark Cutajar <mcut94@gmail.com>
2017-05-26 14:22:55 +0100

    Working on extract_ds.py - Problems due to large amount of data. Commit prior to saving every track

--- pydst/extract_ds.py
+++ pydst/extract_ds.py
@@ -1,28 +1,47 @@
+"""A function that extracts the data from the folders
+    and converts the mp3 files to numpy arrays.
+    
+This function can be used as both a standalone function
+or imported in a different class.
+"""
+
 import csv
 import logging
 import numpy as np
 from pydst import DEFAULT_SEED
 from pydub import AudioSegment
+from time import gmtime, strftime
 
-# Define logger
+# TODO: DRAW HOW DATA IS FLOWING THOUGHT THE FUNCTIONS
+# TODO: CREATE A MOCK DATASET AND TEST IE CREATE A TEST DRIVEN FUNCTION
+
+# Define logger, formatter and handler
 LOGGER_FORMAT = '%(levelname)s:%(asctime)s:%(name)s:%(message)s'
-LOG_FILENAME = 'cre_ds.log'
+TIME = strftime("%Y%m%d_%H%M%S", gmtime())
+LOG_FILENAME = 'cre_ds_'+TIME+'.log'
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 formatter = logging.Formatter(LOGGER_FORMAT)
 file_handler = logging.FileHandler(LOG_FILENAME)
+stream_handler = logging.StreamHandler()
 file_handler.setFormatter(formatter)
+stream_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
+logger.addHandler(stream_handler)
 
 
 def extract_tags_names(root):
-    # Define lists to store data information
+    """Extract the targets, mp3_fules, tids and label_map
+    
+    :param root: the root folder where the files of the
+        magnatagatune dataset is found.
+    :returns: targets, mp3_files, tids, label_map 
+    """
     targets, mp3_files, tids, label_map = [], [], [], []
 
     # Open file and store the information in the appropriate lists
     filename = root + 'annotations_final.csv'
-
     try:
         with open(filename, newline='') as f:
                 annotations_file = csv.reader(f, delimiter='\t')
@@ -37,15 +56,27 @@ def extract_tags_names(root):
                         mp3_files.append(row[-1])
                         targets.append([float(i) for i in row[1:-1]])
     except:
-        logger.error("File: {0} missing.".format(filename))
+        err_str = "File: {0} missing.".format(filename)
+        logger.error(err_str)
+        raise ValueError(err_str)
 
     targets = np.asarray(targets)
     tids = np.asarray(tids)
     mp3_files = np.asarray(mp3_files)
+    label_map = np.asarray(label_map)
     return label_map, mp3_files, targets, tids
 
 
 def extract_data(mp3_filenames, root):
+    """Find each mp3_file from the folder and converts
+        the mp3 file into numpy array.
+    
+    :param mp3_filenames: The name of the mp3 files to 
+        be extracted.
+    :param root: The folder where the magnatagatune 
+        dataset is found.
+    :return: data: Extracted mp3 files in numpy arrays.    
+    """
     data = []
     # Extract mp3 files and convert to numpy arrays
     for mp3_filename in mp3_filenames:
@@ -59,10 +90,52 @@ def extract_data(mp3_filenames, root):
         err_str = "mp3_filenames {} size not equal to extracted data size {}!".format(len(mp3_filenames), data.shape[0])
         logger.error(err_str)
         raise ValueError(err_str)
+    
     return data
 
 
+def sort_tags(targets, labels):
+    """Function to sort the targets and labels according
+        to frequency.
+        
+    :param targets: The targets of the labels to be sorted. 
+    :param labels: The labels of the targets.
+    :return: target: Sorted targets and labels.
+    :return: labels: Sorted labels according to frequency
+        of the targets.
+    """
+    sum_of_targets = np.sum(targets, axis=0)
+    indices = np.flipud(np.argsort(sum_of_targets))
+    sorted_targets = targets[:, indices]
+    sorted_labels = labels[indices]
+    return sorted_targets, sorted_labels
+
+
+def reduction_samples(targets, mp3_files, tids, keep_rows):
+    """Reduction of the number of files to be searched.
+    
+    :param targets
+    :param mp3_files
+    :param tids
+    :param keep_rows 
+    :returns targets, mp3_files, tids, keep_rows
+    """
+    mp3_files = mp3_files[0:keep_rows]
+    targets = targets[0:keep_rows]
+    tids = tids[0:keep_rows]
+    return targets, mp3_files, tids
+
+
 def shuffle(rng, targets, mp3_files, tids):
+    """Shuffle of the targets to be found according to 
+        rng.
+    
+    :param rng: Random generator class.
+    :param targets
+    :param mp3_files 
+    :param tids
+    :returns targets, mp3_files, tids
+    """
     perm = rng.permutation(targets.shape[0])
     ptargets = targets[perm]
     pmp3f = mp3_files[perm]
@@ -71,26 +144,32 @@ def shuffle(rng, targets, mp3_files, tids):
 
 
 def seperate(data, targets, mp3_files, tids, split):
+    """Function to seperate the data according to the 
+        splits defined.
+    
+    :param data
+    :param targets 
+    :param mp3_files 
+    :param tids 
+    :param split: Split array with the fractions of
+        training, validation and test set sizes.
+    :return training_data: Dictionary with the training
+        data.
+    :return validation_data: Dictionary with the valid
+        data.
+    :return test_data: Dictionary with the test data.
+    """
     size_of_dataset = targets.shape[0]
-    test_sz, valid_sz, train_sz = round(size_of_dataset * split[2]), \
-                                  round(size_of_dataset * split[1]), \
-                                  round(size_of_dataset * split[0])
+    test_sz, valid_sz, train_sz = round(size_of_dataset * split[2]), round(size_of_dataset * split[1]), round(
+        size_of_dataset * split[0])
 
-    trn_tgts, trn_data, trn_mp3f, trn_tids = targets[0:train_sz], \
-                                             data[0:train_sz, :], \
-                                             mp3_files[0:train_sz], \
-                                             tids[0:train_sz]
+    trn_tgts, trn_data, trn_mp3f, trn_tids = targets[0:train_sz], data[0:train_sz, :], mp3_files[0:train_sz], tids[0:train_sz]
 
-    vld_tgts, vld_data, vld_mp3f, vld_tids = targets[train_sz:train_sz + valid_sz], \
-                                             data[train_sz:train_sz + valid_sz, :], \
-                                             mp3_files[train_sz:train_sz + valid_sz], \
-                                             tids[train_sz:train_sz + valid_sz]
+    vld_tgts, vld_data, vld_mp3f, vld_tids = targets[train_sz:train_sz + valid_sz], data[train_sz:train_sz + valid_sz, :], \
+                                             mp3_files[train_sz:train_sz + valid_sz], tids[train_sz:train_sz + valid_sz]
 
-
-    tst_tgts, tst_data, tst_mp3f, tst_tids = targets[train_sz + valid_sz: -1], \
-                                             data[train_sz + valid_sz: -1, :], \
-                                             mp3_files[train_sz + valid_sz: -1], \
-                                             tids[train_sz + valid_sz: -1]
+    tst_tgts, tst_data, tst_mp3f, tst_tids = targets[train_sz + valid_sz: -1], data[train_sz + valid_sz: -1, :], \
+                                             mp3_files[train_sz + valid_sz: -1], tids[train_sz + valid_sz: -1]
 
     training_data = {'targets': trn_tgts, 'data': trn_data, 'mp3f': trn_mp3f, 'tids': trn_tids}
     validation_data = {'targets': vld_tgts, 'data': vld_data, 'mp3f': vld_mp3f, 'tids': vld_tids}
@@ -99,20 +178,27 @@ def seperate(data, targets, mp3_files, tids, split):
 
 
 def get_dataset(rng, root, divisions, _size_of = -1):
+    """Function to perform the functions to extract the data.
+    
+    :param rng 
+    :param root 
+    :param divisions 
+    :param _size_of: Number of samples.
+    :returns: trn_data, vld_data, tst_data, label_map
+    """
     # Extract tags and names
     [label_map, mp3_files, targets, tids] = extract_tags_names(root)
     logger.info("Extracted tags and names into arrays")
-    logger.info("Label_map {}, mp3_files {}, targets {}, tids {}".format(len(label_map), len(mp3_files),
-                                                                         targets.shape, tids.shape))
-
+    logger.info("Label_map {}, mp3_files {}, targets {}, tids {}".format(len(label_map), len(mp3_files), targets.shape, tids.shape))
+        
+    # Sample reduction if needed
+    [targets, mp3_files, tids] = reduction_samples(targets, mp3_files, tids, _size_of)
+    logger.info("Number of samples reduced")
+    
     # Sort all targets to be sorted according to frequency
-    # TODO: Create function to sort by frequency
-
-    # Size reduction if needed
-    mp3_files = mp3_files[0:_size_of]
-    targets = targets[0:_size_of]
-    tids = tids[0:_size_of]
-
+    [targets, label_map] = sort_tags(targets, label_map)
+    logger.info("Tags sorted according to frequency")
+    
     # Shuffle names and targets
     [targets, mp3_files, tids] = shuffle(rng, targets, mp3_files, tids)
     logger.info("Shuffled targets, mp3 files and tids")
@@ -132,6 +218,13 @@ def get_dataset(rng, root, divisions, _size_of = -1):
 
 
 def save_archives(trndata,vlddata, tstdata, label_map):
+    """Function to save the data in archives.
+    
+    :param trndata 
+    :param vlddata 
+    :param tstdata 
+    :param label_map
+    """
     train_archive, valid_archive, test_archive = "mtat_train.npz", "mtat_valid.npz", "mtat_test.npz"
 
     np.savez(train_archive, label_map=label_map, mp3_files=trndata['mp3f'], targets=trndata['targets'],
@@ -148,7 +241,7 @@ if __name__ == "__main__":
     # Extract the dataset
     root = "magnatagatune/"
     rng = np.random.RandomState(DEFAULT_SEED)
-    size_of_sets = 5
+    size_of_sets = 10
     divisions = [0.7, 0.1, 0.2]
     [trndata, vlddata, tstdata, label_map] = get_dataset(rng, root, divisions, _size_of=size_of_sets)
     logger.info("Extracted the dataset")
--- pydst/nn_utils.py
+++ pydst/nn_utils.py
@@ -4,36 +4,32 @@
 This module provides utilities classes that are handy in neural network construction.
 """
 
-import os
-import csv
-import time
-import datetime
 import tensorflow as tf
-import numpy as np 
 
-def gloroSTD(params):
-    """Function to return the STD according to the glorot and Bengio (2014)"""
+
+def glorot_std(params):
+    """Function to return the STD    
+    :param params: The input and output layer sizes.
+    :returns Standard deviation according to Glorot and Bengio.
+    """
+
     return (2./(params[0]+params[1]))**0.5
 
-## TODO: transform function that szparams is only the output parameter
-# Fully connected layer function
+
 def fully_connected_layer(inputs, szparams, nonlin=tf.nn.relu, batchNorm=False, keepProb=1):
     """Function to construct a single fully connected layer.
     
-    Args:
-        inputs: Input tensor.
-        szparams: Input and output sizes in a tuple.
-        nonlin: Non-linearity to be used. Default is RELU.
-        BatchNorm: Boolean to apply batch normalization
-        keepProb: Probability to keep a certain weight to be 
+    :param inputs: Input tensor.
+    :param szparams: Input and output sizes in a tuple.
+    :param nonlin: Non-linearity to be used. Default is RELU.
+    :param batchNorm: Boolean to apply batch normalization
+    :param keepProb: Probability to keep a certain weight to be 
             used in the dropout function.
-        
-    Returns:
-        output: Output tensor of layer with transformations.
+    :returns output: Output tensor of layer with transformations.
             -- Affine, (batch), non-linearity, (Dropout) --
     """
-    
-    weights = tf.Variable(tf.truncated_normal([szparams[0], szparams[1]], stddev=gloroSTD(szparams)), name='weights')             
+    # TODO: transform function that szparams is only the output parameter
+    weights = tf.Variable(tf.truncated_normal([szparams[0], szparams[1]], stddev=glorot_std(szparams)), name='weights')
     biases = tf.Variable(tf.zeros([szparams[1]]), name='biases')
     
     affine_output = tf.matmul(inputs, weights) + biases
@@ -41,41 +37,39 @@ def fully_connected_layer(inputs, szparams, nonlin=tf.nn.relu, batchNorm=False, 
     if batchNorm:
         
         epsilon = 0.001
-        gamma = tf.Variable(tf.ones([layerParams[1]]), name='scale')
-        beta = tf.Variable(tf.zeros([layerParams[1]]), name='shift')
+        gamma = tf.Variable(tf.ones([szparams[1]]), name='scale')
+        beta = tf.Variable(tf.zeros([szparams[1]]), name='shift')
         
         mean, var = tf.nn.moments(affine_output, axes=[0], name='moments')
         
-        affine_output = tf.nn.batch_normalization(affine_output, mean, var, beta, gamma, epsilon, name='batch_normalization')
+        affine_output = tf.nn.batch_normalization(affine_output, mean, var, beta, gamma,
+                                                  epsilon, name='batch_normalization')
 
     output = nonlin(affine_output)
     
-    if dropProb!=1:
+    if keepProb != 1:
         output = tf.nn.dropout(output, keepProb)
     return output
 
 
-def multi_fcLayers(inputs, szparams, nonlin=tf.nn.relu, batchNorm=False, keepProb=1):
+def fc_layer_batch(inputs, szparams, nonlin=tf.nn.relu, batchNorm=False, keepProb=1):
     """Function to construct numerous feed forward layers.
     
     Given arrays of sizes, non-linearities, batch normalization
     booleans and keep wieght probabilities; numerous layers are
     constructed.
     
-    Args:
-        inputs: Input tensor.
-        szparams: List with the inputs and outputs of the layers.
-        nonlin: List of the non-linearities of the different
+    :param inputs: Input tensor.
+    :param szparams: List with the inputs and outputs of the layers.
+    :param nonlin: List of the non-linearities of the different
             layers. If a single non-linearity is given, this is 
             transformed into a list.
-        batchNorm: List of batch normalizaton booleans. If a
+    :param batchNorm: List of batch normalizaton booleans. If a
             single boolean is given this is transformed into a
             list.
-        keepProb: List of keep probabilities. If a single boolean
+    :param keepProb: List of keep probabilities. If a single boolean
             is given this is transformed into a list.
-        
-    Return:
-        layer_outs: A list with the outputs of each of the layers.
+        :returns layer_outs: A list with the outputs of each of the layers.
     """
     
     layer_outs = []
@@ -93,31 +87,42 @@ def multi_fcLayers(inputs, szparams, nonlin=tf.nn.relu, batchNorm=False, keepPro
             din = layer_outs[-1]
             
         with tf.name_scope('fc-layer-{0}'.format(lidx)):
-            lout = fully_connected_layer(data_in, sz, nonlin=nonlin[lidx], batchNorm=batchNorm, keepProb=keepProb)
+            lout = fully_connected_layer(din, sz, nonlin=nonlin[lidx], batchNorm=batchNorm, keepProb=keepProb)
             layer_outs.append(lout)
             
     return layer_outs
 
-def define_summaries(scalar_sd={}, histogram_sd={}, image_sd={}):
+
+def define_summaries(scalar_sd = None, histogram_sd = None, image_sd = None ):
     """Function to define summaries
     
     Summaries are provided in dictionaries where the key is used
     for the name of the summary and the value is used as the num
     to be stored.
     
-    Args:
-        scalar_sd: Scalar values to be saved as summaries.
-        histogram_sd: Values to be stored as histograms.
-        image_sd: Values to be stored as images.
-    Returns:
-        merged_summary: The merged summary of saved items.
+    :param scalar_sd: Scalar values to be saved as summaries.
+    :param histogram_sd: Values to be stored as histograms.
+    :param image_sd: Values to be stored as images.
+    :returns merged_summary: The merged summary of saved items.
     """
-    for key, value in scalar_sd.items():
-        tf.summary.scalar(key, value)
-    for key, value in histogram_sd.items():
-        tf.summary.histogram(key, value)
-    for key, value in image_sd.items():
-        tf.summary.image(key, value)
+    if scalar_sd is not None:
+        for key, value in scalar_sd.items():
+            tf.summary.scalar(key, value)
+    if histogram_sd is not None:
+        for key, value in histogram_sd.items():
+            tf.summary.histogram(key, value)
+    if image_sd is not None:
+        for key, value in image_sd.items():
+            tf.summary.image(key, value)
     summary_merged = tf.summary.merge_all()
     return summary_merged
 
+# TODO: Check function on the tensorflow API documentation
+def maxpool_layer(inputs, div=(2,2)):
+    """Function to perform max-pooling on the provided inputs
+    
+    :param inputs: Input tensor.
+    :param div: Check on the tensorflow API documentation to understand the difference between ksize and strides 
+    :return: Max-pooled layer.
+    """
+    return tf.nn.max_pool(inputs, ksize=[1, div[0], div[1], 1], strides=[1, div[0], div[1], 1], padding='SAME')
