abfe493bf2b853f5d688e2682b44008248ccc283
Mark Cutajar <mcut94@gmail.com>
2017-06-07 01:12:16 +0100

    Added setup.py for package details

---
+++ setup.py
@@ -0,0 +1,11 @@
+""" Setup script for pydst package. """
+
+from setuptools import setup
+
+setup(
+    name = "pydst",
+    author = "Mark Cutajar",
+    description = ("Journals, models, and functions used for the msc project"),
+    url = "",
+    packages=['pydst']
+)
