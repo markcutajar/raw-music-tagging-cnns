63b96ec2a6fa08085124eb5d7479ee3dc06925ce
MarkCutajar <mcut94@gmail.com>
2017-06-15 01:50:02 +0100

    Added mlpone - model with 2 mlp layers

--- trainer/models.py
+++ trainer/models.py
@@ -1,11 +1,8 @@
 """Graph functions to test the raw music tagging."""
 
 import tensorflow as tf
-from tensorflow.python.ops import string_ops
 
-
-
-def basic_conv(data_batch,
+def convbasic(data_batch,
                targets_batch,
                learning_rate=0.1):
 
@@ -73,4 +70,62 @@ def basic_conv(data_batch,
 
     summary_op = tf.summary.merge_all()
     
+    return summary_op, train_step, global_step
+
+
+def mlpone(data_batch,
+           targets_batch,
+           learning_rate=0.1):
+    """
+
+    Args:
+        features (Tensor): Matrix input tensor
+        labels (Tensor): Class label tensor
+        learning_rate (float): Learning rate for AdaDeltaOptimizer
+
+    Returns:
+        summary_op: summary to be found
+        train_step: Train step
+        global_step: Global step training
+    """
+
+    outputs = {}
+    errors = {}
+    accs = {}
+
+    name = 'layer-1'
+    outputs[name] = tf.layers.dense(data_batch, 5000, activation=tf.nn.elu)
+
+    name = 'layer-2'
+    outputs[name] = tf.layers.dense(outputs['layer-1'], 1000, activation=tf.nn.elu)
+
+    name = 'layer-3'
+    outputs[name] = tf.layers.dense(outputs['layer-2'], 50, activation=tf.nn.elu)
+    last_output = outputs['layer-3']
+
+    global_step = tf.contrib.framework.get_or_create_global_step()
+
+    name = 'error'
+    with tf.name_scope(name):
+        errors[name] = tf.losses.sigmoid_cross_entropy(multi_class_labels=targets_batch, logits=last_output)
+    tf.summary.scalar(name, errors[name])
+    train_step = tf.train.AdadeltaOptimizer(learning_rate=learning_rate).minimize(errors[name], global_step=global_step)
+
+    """name = 'false_negatives'
+    with tf.name_scope(name):
+        accs[name] = tf.metrics.false_negatives(labels=targets_batch, predictions=last_output)
+    tf.summary.scalar(name, accs[name])"""
+
+    """name = 'false_positives'
+    with tf.name_scope(name):
+        accs[name] = tf.metrics.false_positives(labels=targets_batch, predictions=last_output)
+    tf.summary.scalar(name, accs[name])"""
+
+    """name = 'sensitivity'
+    with tf.name_scope(name):
+        accs[name] = tf.metrics.precision(labels=targets_batch, predictions=last_output)
+    tf.summary.scalar(name, accs[name])"""
+
+    summary_op = tf.summary.merge_all()
+
     return summary_op, train_step, global_step
