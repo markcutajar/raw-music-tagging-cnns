3bffb86ed5b684540c4243b1de773fd45d49d79e
MarkCutajar <mcut94@gmail.com>
2017-06-17 13:17:51 +0100

    Added new dataprovider using tfrecords - working - no FBanks yet!

--- pydst/dataproviders.py
+++ pydst/dataproviders.py
@@ -1,228 +1,127 @@
 # -*- coding: utf-8 -*-
 """Data providers.
-This module provides classes for loading datasets and iterating over batches of
-data points.
+This module provides classes for loading data and iterating over batches of
+data points using TensorFlow's queue runners.
 
-TODO: Logger and tags and docstrings
+TODO: docstrings
 """
-
 import os
-import threading
-import numpy as np
+import json
 import tensorflow as tf
-DEFAULT_SEED = 12345
+import multiprocessing
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 
 class DataProvider(object):
-    """Data Provider from files and metadata"""
-    
-    def __init__(self, which_set, batch_size=20, target_size=-1, num_samples=131072, max_samples=465984, data_depth=1,
-                shuffle_order=True, rng=None, root='../magnatagatune/dataset/raw/', shape='flat'):
-        
-        """Create a new data provider object.
 
-        Args:
-            inputs (ndarray): Array of data input features of shape
-                (num_data, input_dim).
-            targets (ndarray): Array of data output targets of shape
-                (num_data, output_dim) or (num_data,) if output_dim == 1.
-            batch_size (int): Number of data points to include in each batch.
-            max_num_batches (int): Maximum number of batches to iterate over
-                in an epoch. If `max_num_batches * batch_size > num_data` then
-                only as many batches as the data can be split into will be
-                used. If set to -1 all of the data will be used.
-            shuffle_order (bool): Whether to randomly permute the order of
-                the data before each epoch.
-            rng (RandomState): A seeded random number generator.
-            
-            TODO: FINISH DOCSTRING
+    def __init__(self,
+                 filenames,
+                 metadata_file,
+                 batch_size=20,
+                 num_epochs=None,
+                 num_tags=None,
+                 selective_tags=None,
+                 num_samples=None,
+                 data_shape='image',
+                 shuffle=True):
+
+        """Class to load the data and provide batches to
+        the calling function. Every run a batch is returned.
+        Functions include either tfr or csv.
+
+        :param filenames: File name and location of tfrecord
+        :param batch_size: The size of batches to be provided
+        :param num_epochs: The number of epochs to be returned. If None,
+            and indefinite number is provided.
+        :param num_tags: The number of tags in the target tensor
+        :param selective_tags: If specific names are given, targets
+            provided in the list only are returned.
+        :param num_samples: Number of samples in the feature tensors
+        :param data_shape: The shape of the data. 'flat' for fully
+            connected networks and 'image' for convolutional inputs.
+        :param shuffle: Boolean whether to shuffle the batches or not
+        :return:
         """
-        
-        MAX_TAGS = 188
-        
-        # Check valid argument data
-        assert which_set in ['train', 'valid', 'test'], (
-            'Expected which_set to be either train, valid or test. '
-            'Got {}'.format(which_set))
-
-        # Set data directory paths
-        self.data_dir = os.path.join(root, 'tracks/')
-        self.metad_path = os.path.join(root, (which_set + '_metadata.npz'))
-        assert os.path.isfile(self.metad_path), (
-            'Metadata file does not exist at expected path: ' + self.metad_path)
-        assert os.path.isdir(self.data_dir), (
-            'Data directory does not exist at expected path: ' + self.data_dir)
-        
-        # Load targets and tids
-        metad = np.load(self.metad_path)
-        self.targets = metad['targets']
-        self.tids = metad['tids']
-        
-        if target_size < 1 and target_size != -1:
-            raise ValueError('target_size must be -1 or > 0')  
-        # Set number of tags
-        if target_size == -1:
-            self._target_size = MAX_TAGS
-        else:
-            self._target_size = min(target_size, MAX_TAGS)
-     
-    
-        # Set batch information
-        if batch_size < 1:
-            raise ValueError('batch_size must be >= 1')
         self._batch_size = batch_size
-        self._max_num_batches = int(np.floor(self.tids.shape[0] / self._batch_size))
-        self._maxq = self._batch_size * 5
-        
-        
-        # Set shape of output
-        assert shape in ['flat', 'image'], (
-            'Expected shape to be either flat or image. '
-            'Got {}.'.format(shape))
-          
-        samples_to_remove = max_samples - num_samples
-        if samples_to_remove < 0:
-            raise ValueError('num_samples {} is larger than max_samples {}'.format(num_samples, max_samples))
-        
-        if shape == 'flat':
-            self.data_out_dim = [self._batch_size, num_samples * data_depth]
-            self.queue_shape = [num_samples * data_depth]    
-        elif shape == 'image' and data_depth == 1:
-            self.data_out_dim = [self._batch_size, num_samples, 1]
-            self.queue_shape = [num_samples, 1]
+        self._num_tags = num_tags
+        self._selective_tags = selective_tags
+        self._num_samples = num_samples
+        self._data_shape = data_shape
+        self._shuffle = shuffle
+
+        with open(metadata_file, 'r') as f:
+            metadata = json.load(f)
+
+        self._label_map = metadata['label_map']
+        self._max_samples = metadata['max_num_samples']
+        self._max_tags = metadata['max_num_tags']
+        self._sample_depth = metadata['sample_depth']
+
+        self._filename_queue = tf.train.string_input_producer(
+            filenames, num_epochs=num_epochs)
+
+    def raw_input_fn(self):
+
+        # Load the data from the records
+        reader = tf.TFRecordReader()
+        _, serialized_example = reader.read_up_to(self._filename_queue,
+                                                  num_records=self._batch_size)
+        features = tf.parse_example(
+            serialized_example,
+            features={
+                'num_samples': tf.FixedLenFeature([], tf.int64),
+                'sample_depth': tf.FixedLenFeature([], tf.int64),
+                'num_tags': tf.FixedLenFeature([], tf.int64),
+                'tags': tf.FixedLenFeature([], tf.string),
+                'song': tf.FixedLenFeature([], tf.string)
+            }
+        )
+        all_song = tf.decode_raw(features['song'], tf.int32)
+        all_tags = tf.decode_raw(features['tags'], tf.int32)
+
+        # Reduce tags or selective
+        if self._selective_tags is not None:
+            raise NotImplementedError('Selective tags not implemented yet!')
         else:
-            self.data_out_dim = [self._batch_size, num_samples, data_depth, 1]
-            self.queue_shape = [num_samples, data_depth, 1]
-        
-        self._right_snip = int(np.round((samples_to_remove)/ 2.0))
-        self._left_snip = (samples_to_remove) - self._right_snip
-        self._data_depth = data_depth
+            if self._num_tags is None or self._num_tags == -1:
+                target_size = self._max_tags
+            elif self._num_tags > 0:
+                target_size = min(self._num_tags, self._max_tags)
+            else:
+                raise ValueError('target_size must be -1 or > 0')
+            tags = tf.slice(all_tags, [0, 0], [-1, target_size])
 
-        # Shuffle if needed
-        if rng is None:
-            rng = np.random.RandomState(DEFAULT_SEED)
-        self.rng = rng             
-        if shuffle_order:
-            self.shuffle()  
-        
-        # Initialize queue
-        self.init_queue()
+        # Reduce samples
+        if self._num_samples is None or self._num_samples == -1:
+            num_samples = self._max_samples
+        elif self._num_samples > 1:
+            num_samples = min(self._num_samples, self._max_samples)
+        else:
+            raise ValueError('Number of samples must be -1 or > 0')
+        if self._sample_depth == 1:
+            song = tf.slice(all_song, [0, 0], [-1, num_samples])
+        else:
+            raise NotImplementedError
 
-        
-    def init_queue(self):
-        """Initialises queue using tensorflow FIFOQueue"""
-        self.q_din = tf.placeholder(tf.float32, shape=self.data_out_dim)
-        self.q_tin = tf.placeholder(tf.float32, shape=[self._batch_size, self._target_size])
+        # Shuffle
+        if self._shuffle:
+            song, tags = tf.train.shuffle_batch(
+                [song, tags],
+                batch_size=self._batch_size,
+                capacity=self._batch_size*10,
+                num_threads=multiprocessing.cpu_count(),
+                min_after_dequeue=10,
+                enqueue_many=True)
 
-            # Set queue with FIFOQueue or RandomShuffleQueue
-            self.q = tf.FIFOQueue(shapes=[self.queue_shape, [self._target_size]], 
-                                                              capacity=self._maxq, dtypes=[tf.float32, tf.float32])        
-
-            self.enqop = self.q.enqueue_many([self.q_din, self.q_tin])
-        
-        
-    def get_data(self):
-        """Function to dequeue and return batches"""
-        data_batch, targets_batch= tf.train.batch(self.q.dequeue(), batch_size=self._batch_size, capacity=self._maxq)
-        return data_batch, targets_batch
-    
-    
-    def enable(self, sess, num_threads=1):
-        threads = []
-        self.shuffle()
-        for idx in range(num_threads):
-            thread = threading.Thread(target=self.load_q, args=(sess,))
-            thread.daemon = True
-            thread.start()
-            threads.append(thread)
-        return threads
-        
-    
-    def disable(self, sess):
-            sess.run(self.q.close(cancel_pending_enqueues=True))
-            
-    def load_q(self, sess):
-        """Function to enqueue data in the object queue
-        
-        The data needs to be loaded using the tids.
-        """
-        maxp = self.mtids.shape[0]
-        lowp = 0
-        topp = 0
-        
-        while True:
-            while topp != None:
-
-                topp = lowp + self._batch_size
-                if topp == maxp:
-                    topp = None
-
-                toload = self.mtids[lowp:topp]
-                ctargets = self.mtargets[lowp:topp]
-                if (self._target_size-self.mtargets.shape[1]) < 0:
-                    ctargets = np.delete(ctargets, np.s_[(self._target_size-self.mtargets.shape[1])::], axis=1)
-
-                for idx, tid in enumerate(toload):
-                    loaded = np.load(self.data_dir + str(tid) + '.npy')
-                    
-                    if len(self.data_out_dim) == 2:
-                        loaded = np.expand_dims(loaded.reshape((-1)), axis=0)
-                        if self._left_snip != 0:
-                            loaded = np.delete(loaded, np.s_[:(self._left_snip * self._data_depth)], axis=1)
-                        if self._right_snip != 0:
-                            loaded = np.delete(loaded, np.s_[(-self._data_depth * self._right_snip):], axis=1) 
-                    elif len(self.data_out_dim) == 3:
-                        loaded = np.expand_dims(loaded.reshape((-1, 1)), axis=0)
-                        if self._left_snip != 0:
-                            loaded = np.delete(loaded, np.s_[:(self._left_snip)], axis=1)
-                        if self._right_snip != 0:
-                            loaded = np.delete(loaded, np.s_[(-self._right_snip):], axis=1)
-                    else:
-                        loaded = np.expand_dims(np.expand_dims(loaded, axis=0), axis=3)
-                        if self._left_snip != 0:
-                            loaded = np.delete(loaded, np.s_[:(self._left_snip)], axis=1)
-                        if self._right_snip != 0:
-                            loaded = np.delete(loaded, np.s_[(-self._right_snip):], axis=1)
-                        
-                    if idx == 0:
-                        cdata = loaded
-                    else:
-                        cdata = np.concatenate((cdata, loaded), axis=0)
-
-                lowp = topp
-                
-                try:
-                    sess.run(self.enqop, feed_dict={self.q_din: cdata, self.q_tin: ctargets})
-                except tf.errors.CancelledError:
-                    return
-            
-            self.shuffle()
-            lowp = 0
-            topp = 0
-            
-            
-    def shuffle(self):
-        """Randomly shuffles order of data."""          
-        perm = self.rng.permutation(self.tids.shape[0])
-        self.tids = self.tids[perm]
-        self.targets = self.targets[perm, :]
-        
-        self.mtids = self.tids[0:self._max_num_batches * self._batch_size]
-        self.mtargets = self.targets[0:self._max_num_batches * self._batch_size] 
-        
-            
-    def batch_size(self):
-        """Returns the _batch_size."""
-        return self._batch_size
-    
-    
-    def target_size(self):
-        """Returns the _target_size."""
-        return self._target_size
-    
-    
-    def max_num_batches(self):
-        """Returns the _max_num_batches."""
-        return self._max_num_batches
-
-    
+        # Return depending on shape
+        if self._data_shape == 'flat':
+            return song, tags
+        elif self._data_shape == 'image':
+            if self._sample_depth == 1:
+                song = tf.expand_dims(song, axis=2)
+            else:
+                raise NotImplementedError
+            return song, tags
+        else:
+            raise ValueError('Shape not recognized!')
--- trainer/dataproviders.py
+++ trainer/dataproviders.py
@@ -3,24 +3,26 @@
 This module provides classes for loading data and iterating over batches of
 data points using TensorFlow's queue runners.
 
-TODO: Logger and tags and docstrings
+TODO: docstrings
 """
-
+import os
+import json
 import tensorflow as tf
 import multiprocessing
-import numpy as np
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 
-class BatchInputFunctions(object):
+class DataProvider(object):
 
     def __init__(self,
                  filenames,
-                 label_map_file,
+                 metadata_file,
                  batch_size=20,
                  num_epochs=None,
-                 num_tags=50,
+                 num_tags=None,
                  selective_tags=None,
-                 num_samples=131072,
+                 num_samples=None,
                  data_shape='image',
                  shuffle=True):
 
@@ -41,25 +43,29 @@ class BatchInputFunctions(object):
         :param shuffle: Boolean whether to shuffle the batches or not
         :return:
         """
-
-        self._filenames = filenames
         self._batch_size = batch_size
-        self._num_epochs = num_epochs
         self._num_tags = num_tags
         self._selective_tags = selective_tags
         self._num_samples = num_samples
         self._data_shape = data_shape
-        self._shuffle = shuffl
+        self._shuffle = shuffle
 
-        self._label_map = np.load(label_map_file)['label_map'].tolist()
+        with open(metadata_file, 'r') as f:
+            metadata = json.load(f)
 
-    def tfr_input_fn(self):
-        filename_queue = tf.train.string_input_producer(
-            self._filenames, num_epochs=self._num_epochs)
+        self._label_map = metadata['label_map']
+        self._max_samples = metadata['max_num_samples']
+        self._max_tags = metadata['max_num_tags']
+        self._sample_depth = metadata['sample_depth']
 
+        self._filename_queue = tf.train.string_input_producer(
+            filenames, num_epochs=num_epochs)
+
+    def raw_input_fn(self):
+
+        # Load the data from the records
         reader = tf.TFRecordReader()
-
-        _, serialized_example = reader.read_up_to(filename_queue,
+        _, serialized_example = reader.read_up_to(self._filename_queue,
                                                   num_records=self._batch_size)
         features = tf.parse_example(
             serialized_example,
@@ -71,62 +77,51 @@ class BatchInputFunctions(object):
                 'song': tf.FixedLenFeature([], tf.string)
             }
         )
-
         all_song = tf.decode_raw(features['song'], tf.int32)
         all_tags = tf.decode_raw(features['tags'], tf.int32)
-
-        max_num_samples = tf.cast(features['num_samples'], tf.int64)
-        samples_depth = tf.cast(features['sample_depth'], tf.int64)
-        max_num_tags = tf.cast(features['num_tags'], tf.int64)
 
         # Reduce tags or selective
         if self._selective_tags is not None:
             raise NotImplementedError('Selective tags not implemented yet!')
         else:
             if self._num_tags is None or self._num_tags == -1:
-                tags = all_tags
+                target_size = self._max_tags
             elif self._num_tags > 0:
-                target_size = min(self._num_tags, max_num_tags)
-                tags = tf.slice(all_tags, [0, 0], [-1, target_size])
+                target_size = min(self._num_tags, self._max_tags)
             else:
                 raise ValueError('target_size must be -1 or > 0')
+            tags = tf.slice(all_tags, [0, 0], [-1, target_size])
 
         # Reduce samples
-        song = all_song
         if self._num_samples is None or self._num_samples == -1:
-            pass
+            num_samples = self._max_samples
         elif self._num_samples > 1:
-            num_samples = min(self._num_samples, max_num_samples)
-            if samples_depth == 1:
-                song = tf.slice(all_song, [0, 0], [-1, num_samples])
-            else:
-                raise NotImplementedError('Not implemented for FBanks!')
+            num_samples = min(self._num_samples, self._max_samples)
+        else:
+            raise ValueError('Number of samples must be -1 or > 0')
+        if self._sample_depth == 1:
+            song = tf.slice(all_song, [0, 0], [-1, num_samples])
+        else:
+            raise NotImplementedError
 
         # Shuffle
         if self._shuffle:
-            # This operation builds up a buffer of rows so that, even between batches,
-            # rows are fed to training in a suitably randomized order.
-            song = tf.train.shuffle_batch(
-                song,
-                self._batch_size,
-                capacity=self._batch_size * 10,
-                min_after_dequeue=self._batch_size * 2 + 1,
+            song, tags = tf.train.shuffle_batch(
+                [song, tags],
+                batch_size=self._batch_size,
+                capacity=self._batch_size*10,
                 num_threads=multiprocessing.cpu_count(),
-                enqueue_many=True,
-                allow_smaller_final_batch=True
-            )
+                min_after_dequeue=10,
+                enqueue_many=True)
 
         # Return depending on shape
         if self._data_shape == 'flat':
             return song, tags
         elif self._data_shape == 'image':
-            if samples_depth == 1:
+            if self._sample_depth == 1:
                 song = tf.expand_dims(song, axis=2)
-                return song, tags
             else:
-                raise NotImplementedError('Not implemented for FBanks!')
+                raise NotImplementedError
+            return song, tags
         else:
             raise ValueError('Shape not recognized!')
-
-    def csv_input_fn(self):
-        raise NotImplementedError('Input from CSV not implemented yet!')
