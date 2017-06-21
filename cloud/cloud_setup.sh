90fbf8161ec154dbc9ab1e31a17e224f76cc671c
MarkCutajar <mcut94@gmail.com>
2017-06-21 19:50:19 +0100

    Added scripts for local and cloud setup

---
+++ cloud/cloud_setup.sh
@@ -0,0 +1,8 @@
+MODEL=dielemanschrauwen256
+JOB_NAME=ds256_04
+JOB_DIR=gs://magnatagatune_dataset/output_ds256_04
+TRAIN_FILE=gs://magnatagatune_dataset/train_rawdata.tfrecords
+EVAL_FILE=gs://magnatagatune_dataset/valid_rawdata.tfrecords
+TRAIN_STEPS=22000
+REGION=us-east1
+CONFIG=config.yaml
---
+++ cloud/local_setup.sh
@@ -0,0 +1,5 @@
+MODEL=dielemanschrauwen256
+JOB_DIR=output_ds256_03
+TRAIN_FILE=../magnatagatune/train_rawdata.tfrecords
+EVAL_FILE=../magnatagatune/valid_rawdata.tfrecords
+TRAIN_STEPS=4000
