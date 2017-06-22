e626502701fa3312d6dc09859db4aab65702ba40
MarkCutajar <mcut94@gmail.com>
2017-06-22 14:12:42 +0100

    Modified local setup to accept selective tags

--- cloud/local_setup.sh
+++ cloud/local_setup.sh
@@ -1,5 +1,6 @@
 MODEL=dielemanschrauwen256
-JOB_DIR=output_ds256_03
+JOB_DIR=output_ds256_05
 TRAIN_FILE=../magnatagatune/train_rawdata.tfrecords
 EVAL_FILE=../magnatagatune/valid_rawdata.tfrecords
 TRAIN_STEPS=4000
+SELTAGS=../magnatagatune/selective_tags.json
