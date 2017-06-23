0f034dfc4869485555bb4d9ba2dbc30ae6f38121
MarkCutajar <mcut94@gmail.com>
2017-06-23 16:12:19 +0100

    Model with scope for visualization purposes

--- cloud/trainer/models.py
+++ cloud/trainer/models.py
@@ -4,18 +4,16 @@ import tensorflow as tf
 
 TRAIN, EVAL, PREDICT = 'TRAIN', 'EVAL', 'PREDICT'
 
-
 def dielemanschrauwen256(data_batch):
     
     outputs = {}
-
     name = 'strided-conv'
-    outputs[name] = tf.layers.conv1d(data_batch, 1, 256, strides=256, activation=tf.nn.relu,
+    outputs[name] = tf.layers.conv1d(data_batch, 1, 3, strides=3, activation=tf.nn.relu,
                                      kernel_regularizer=tf.layers.batch_normalization,
                                      name=name)
 
     name = 'conv-1'
-    outputs[name] = tf.layers.conv1d(outputs['strided-conv'], 32, 8, strides=1, activation=tf.nn.relu,
+    outputs[name] = tf.layers.conv1d(outputs['strided-conv'], 2, 8, strides=1, activation=tf.nn.relu,
                                      kernel_regularizer=tf.layers.batch_normalization,
                                      name=name)
 
@@ -23,48 +21,52 @@ def dielemanschrauwen256(data_batch):
     outputs[name] = tf.layers.max_pooling1d(outputs['conv-1'], pool_size=4, strides=1, name=name)
 
     name = 'conv-2'
-    outputs[name] = tf.layers.conv1d(outputs['maxp-1'], 32, 8, strides=1, activation=tf.nn.relu,
+    outputs[name] = tf.layers.conv1d(outputs['maxp-1'], 2, 8, strides=1, activation=tf.nn.relu,
                                      kernel_regularizer=tf.layers.batch_normalization,
                                      name=name)
 
     name = 'maxp-2'
-    outputs[name] = tf.layers.max_pooling1d(outputs['conv-2'], pool_size=4, strides=1, name=name)
+    outputs[name] = tf.layers.max_pooling1d(outputs['conv-2'], pool_size=2, strides=1, name=name)
+
+    name = 'flatten'
+    outputs[name] = tf.reshape(outputs['maxp-2'], [int(outputs['maxp-2'].shape[0]), -1])
 
     name = 'fcl-1'
-    outputs[name] = tf.layers.dense(tf.reshape(outputs['maxp-2'],
-                                               [int(outputs['maxp-2'].shape[0]), -1]),
-                                    100, activation=tf.nn.relu, name=name)
+    outputs[name] = tf.layers.dense(outputs['flatten'], 100, activation=tf.nn.relu, name=name)
 
     name = 'fcl-2'
     outputs[name] = tf.layers.dense(outputs['fcl-1'], 29, activation=tf.identity, name=name)
-
     return outputs[name]
 
 
-def model_controller(function_name,
-                     mode,
-                     data_batch,
-                     targets_batch,
-                     learning_rate=0.1,
-                     window_size=False):
+def controller(function_name,
+               mode,
+               data_batch,
+               targets_batch,
+               learning_rate=0.1,
+               window=False):
 
     # Load model
-    model = getattr(globals()['models'](), function_name)
+    model = globals()[function_name]
 
     # Get logits depending if windowed inputs or not
-    if window_size:
-        logits_array = tf.map_fn(lambda window: model(window),
-                                 elems=data_batch,
-                                 back_prop=True,
-                                 name='Mapping for window predictions')
-        logits = tf.concat(logits_array, axis=0, name='concat of windowed logits')
+    if window:
+        with tf.name_scope('windowed_model'):
+            logits_array = tf.map_fn(lambda w: model(w),
+                                     elems=data_batch,
+                                     back_prop=True,
+                                     name='MapModels')
+
+            logits = tf.concat(logits_array, axis=0, name='windowLogits')
+            logits = tf.reduce_mean(logits, axis=0, name='averageLogits')
     else:
-        # Get logits with one call
-        logits = model(data_batch)
+            # Get logits with one call
+            logits = model(data_batch)
 
     # Get tag predictions
-    prediction_values = tf.nn.sigmoid(logits, name='probabilities')
-    predictions = tf.round(prediction_values)
+    with tf.name_scope('preds') as scope:
+        prediction_values = tf.nn.sigmoid(logits, name='probs')
+        predictions = tf.round(prediction_values)
 
     # Get errors and accuracies
     if mode in (TRAIN, EVAL):
@@ -75,26 +77,28 @@ def model_controller(function_name,
                 multi_class_labels=targets_batch, logits=logits)
 
         if mode in TRAIN:
-            train_step = tf.train.AdamOptimizer().minimize(error, global_step=global_step)
+            with tf.name_scope('train') as scope:
+                train_step = tf.train.AdamOptimizer().minimize(error, global_step=global_step)
             return train_step, global_step, error
 
         else:
-            streaming_metrics = {
-                'false_negatives': tf.contrib.metrics.streaming_false_negatives(
-                    predictions, targets_batch, name='false_negatives'),
-                'false_positives': tf.contrib.metrics.streaming_false_positives(
-                    predictions, targets_batch, name='false_positives'),
-                'true_positives': tf.contrib.metrics.streaming_true_positives(
-                    predictions, targets_batch, name='true_positives'),
-                'true_negatives': tf.contrib.metrics.streaming_true_negatives(
-                    predictions, targets_batch, name='true_negatives'),
-                'precision': tf.contrib.metrics.streaming_precision(
-                    predictions, targets_batch, name='precision'),
-                'auc': tf.contrib.metrics.streaming_auc(
-                    predictions, targets_batch, name='auc'),
-                'accuracy': tf.contrib.metrics.streaming_accuracy(
-                    predictions, targets_batch, name='accuracy')
-            }
+            with tf.name_scope('metrics') as scope:
+                streaming_metrics = {
+                    'false_negatives': tf.contrib.metrics.streaming_false_negatives(
+                        predictions, targets_batch, name='false_negatives'),
+                    'false_positives': tf.contrib.metrics.streaming_false_positives(
+                        predictions, targets_batch, name='false_positives'),
+                    'true_positives': tf.contrib.metrics.streaming_true_positives(
+                        predictions, targets_batch, name='true_positives'),
+                    'true_negatives': tf.contrib.metrics.streaming_true_negatives(
+                        predictions, targets_batch, name='true_negatives'),
+                    'precision': tf.contrib.metrics.streaming_precision(
+                        predictions, targets_batch, name='precision'),
+                    'auc': tf.contrib.metrics.streaming_auc(
+                        predictions, targets_batch, name='auc'),
+                    'accuracy': tf.contrib.metrics.streaming_accuracy(
+                        predictions, targets_batch, name='accuracy')
+                }
             return streaming_metrics, error
 
     elif mode in PREDICT:
