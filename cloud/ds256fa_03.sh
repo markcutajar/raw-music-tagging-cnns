bb90c03e0a874f3a257ca277ae5ffa296a56baa5
MarkCutajar <mcut94@gmail.com>
2017-07-10 14:45:48 +0100

    Added ds256 running scripts

---
+++ cloud/ds256fa_03.sh
@@ -0,0 +1,35 @@
+TEST_SCRIPT_NAME=ds256fa_03
+MODEL=ds256fa
+
+current_date=$(date +%m%d_%H%M)
+JOB_NAME=${TEST_SCRIPT_NAME}_${current_date}
+JOB_DIR=gs://magnatagatune_dataset/out_$JOB_NAME
+TRAIN_FILE=gs://magnatagatune_dataset/train_win_fbanksdata.tfrecords
+EVAL_FILE=gs://magnatagatune_dataset/valid_win_fbanksdata.tfrecords
+METADATA_FILE=gs://magnatagatune_dataset/fbank40_win_metadata.json
+TRAIN_STEPS=20000
+LEARNING_RATE=0.01
+EVAL_EPOCHS=1
+EVAL_STEPS=106
+REGION=us-east1
+CONFIG=config.yaml
+WINDOWING=SPM
+
+gcloud ml-engine jobs submit training $JOB_NAME \
+--stream-logs \
+--runtime-version 1.2 \
+--job-dir $JOB_DIR \
+--module-name trainer.task \
+--package-path trainer/ \
+--region $REGION \
+--config $CONFIG \
+-- \
+--train-files $TRAIN_FILE \
+--eval-files $EVAL_FILE \
+--train-steps $TRAIN_STEPS \
+--eval-steps $EVAL_STEPS \
+--eval-num-epochs $EVAL_EPOCHS \
+--metadata-files $METADATA_FILE \
+--learning-rate $LEARNING_RATE \
+--windowing-type $WINDOWING \
+--model-function $MODEL
