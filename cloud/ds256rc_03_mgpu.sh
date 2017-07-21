168d5139a859133dcdb5ef8d65cfcf316264f49c
MarkCutajar <mcut94@gmail.com>
2017-07-21 11:23:33 +0100

    Added GPU scripts

---
+++ cloud/ds256rc_03_mgpu.sh
@@ -0,0 +1,43 @@
+TEST_SCRIPT_NAME=ds256ra_03
+MODEL=ds256rc
+
+current_date=$(date +%m%d_%H%M)
+JOB_NAME=${TEST_SCRIPT_NAME}_${current_date}
+JOB_DIR=gs://magnatagatune_dataset/out_$JOB_NAME
+
+TRAIN_FILE=gs://magnatagatune_dataset/train_win_rawdata.tfrecords
+EVAL_FILE=gs://magnatagatune_dataset/valid_win_rawdata.tfrecords
+METADATA_FILE=gs://magnatagatune_dataset/raw_win_metadata.json
+
+TRAIN_STEPS=11200
+LEARNING_RATE=0.1
+EVAL_EPOCHS=1
+EVAL_STEPS=106
+EVAL_BATCH=48
+TRAIN_BATCH=26
+
+REGION=us-east1
+CONFIG=config.yaml
+
+WINDOWING=SPM
+
+gcloud ml-engine jobs submit training $JOB_NAME \
+--stream-logs \
+--runtime-version 1.2 \
+--job-dir $JOB_DIR \
+--module-name trainer.task_mgpu \
+--package-path trainer/ \
+--region $REGION \
+--config $CONFIG \
+-- \
+--train-files $TRAIN_FILE \
+--eval-files $EVAL_FILE \
+--train-steps $TRAIN_STEPS \
+--eval-steps $EVAL_STEPS \
+--eval-num-epochs $EVAL_EPOCHS \
+--eval-batch-size $EVAL_BATCH \
+--train-batch-size $TRAIN_BATCH \
+--metadata-files $METADATA_FILE \
+--learning-rate $LEARNING_RATE \
+--windowing-type $WINDOWING \
+--model-function $MODEL
